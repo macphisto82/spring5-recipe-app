ed9070734ff6dc0304a9c1633f02101ca141feaa
macphisto82 <alessandro.pilonato@gmail.com>
2019-01-28 21:43:15 +0100

    Added jpa enumeration

---
+++ src/main/java/ap/springframework/domain/Difficulty.java
@@ -0,0 +1,5 @@
+package ap.springframework.domain;
+
+public enum Difficulty {
+	EASY, MODEARATE, KIND_OF_HARD, HARD
+}
--- src/main/java/ap/springframework/domain/Recipe.java
+++ src/main/java/ap/springframework/domain/Recipe.java
@@ -4,6 +4,8 @@ import java.util.Set;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -24,8 +26,8 @@ public class Recipe {
 	private String source;
 	private String url;
 	private String directions;
-	//todo add
-    //private Difficulty difficulty;
+	@Enumerated( value = EnumType.STRING)
+    private Difficulty difficulty;
 	@Lob
     private Byte[] image;
     
@@ -34,73 +36,100 @@ public class Recipe {
     
 	@OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
 	private Set<Ingredient> ingredients;
-    
-    public Long getId() {
+
+	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
-	
+
 	public String getDescription() {
 		return description;
 	}
+
 	public void setDescription(String description) {
 		this.description = description;
 	}
+
 	public Integer getPrepTime() {
 		return prepTime;
 	}
+
 	public void setPrepTime(Integer prepTime) {
 		this.prepTime = prepTime;
 	}
+
 	public Integer getCookTime() {
 		return cookTime;
 	}
+
 	public void setCookTime(Integer cookTime) {
 		this.cookTime = cookTime;
 	}
+
 	public Integer getServings() {
 		return servings;
 	}
+
 	public void setServings(Integer servings) {
 		this.servings = servings;
 	}
+
 	public String getSource() {
 		return source;
 	}
+
 	public void setSource(String source) {
 		this.source = source;
 	}
+
 	public String getUrl() {
 		return url;
 	}
+
 	public void setUrl(String url) {
 		this.url = url;
 	}
+
 	public String getDirections() {
 		return directions;
 	}
+
 	public void setDirections(String directions) {
 		this.directions = directions;
 	}
+
+	public Difficulty getDifficulty() {
+		return difficulty;
+	}
+
+	public void setDifficulty(Difficulty difficulty) {
+		this.difficulty = difficulty;
+	}
+
 	public Byte[] getImage() {
 		return image;
 	}
+
 	public void setImage(Byte[] image) {
 		this.image = image;
 	}
+
 	public Notes getNotes() {
 		return notes;
 	}
+
 	public void setNotes(Notes notes) {
 		this.notes = notes;
 	}
+
 	public Set<Ingredient> getIngredients() {
 		return ingredients;
 	}
+
 	public void setIngredients(Set<Ingredient> ingredients) {
 		this.ingredients = ingredients;
 	}
-
 }
