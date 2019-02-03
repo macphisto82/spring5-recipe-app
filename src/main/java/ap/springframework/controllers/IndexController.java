ad266788302ba4f76a8896c1b8f85ebe6622eba5
macphisto82 <alessandro.pilonato@gmail.com>
2019-02-03 18:59:23 +0100

    added jpa queries

--- src/main/java/ap/springframework/controllers/IndexController.java
+++ src/main/java/ap/springframework/controllers/IndexController.java
@@ -1,13 +1,32 @@
 package ap.springframework.controllers;
 
+import java.util.Optional;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import ap.springframework.domain.Category;
+import ap.springframework.domain.UnitOfMeasure;
+import ap.springframework.repositories.CategoryRepository;
+import ap.springframework.repositories.UnitOfMeasureRepository;
 
 @Controller
 public class IndexController {
 
+	private CategoryRepository categoryRepository;
+	private UnitOfMeasureRepository unitOfMeasureRepository;
+	
+	public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+		this.categoryRepository = categoryRepository;
+		this.unitOfMeasureRepository = unitOfMeasureRepository;
+	}
+
 	@RequestMapping({"","/","/index"})
 	public String getIndexPage() {
+		Optional<Category> categOptional = categoryRepository.findByDescription("American");
+		Optional<UnitOfMeasure> unitOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
+		System.out.println("Category id: " + categOptional.get().getId());
+		System.out.println("UoM id: " + unitOptional.get().getId());
 		return "index";
 	}
 	
--- src/main/java/ap/springframework/repositories/UnitOfMeasureRepository.java
+++ src/main/java/ap/springframework/repositories/UnitOfMeasureRepository.java
@@ -1,9 +1,13 @@
 package ap.springframework.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 
 import ap.springframework.domain.UnitOfMeasure;
 
 public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure, Long>{
+	
+	Optional<UnitOfMeasure> findByDescription(String description);
 
 }
