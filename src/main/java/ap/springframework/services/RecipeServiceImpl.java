40195c6bedb5689d0af58ab8a10cd1f388054b1d
macphisto82 <alessandro.pilonato@gmail.com>
2019-02-12 11:08:01 +0100

    added lombock annotations

--- src/main/java/ap/springframework/services/RecipeServiceImpl.java
+++ src/main/java/ap/springframework/services/RecipeServiceImpl.java
@@ -7,7 +7,9 @@ import org.springframework.stereotype.Service;
 
 import ap.springframework.domain.Recipe;
 import ap.springframework.repositories.RecipeRepository;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Service
 public class RecipeServiceImpl implements RecipeService {
 
@@ -19,6 +21,7 @@ public class RecipeServiceImpl implements RecipeService {
 
 	@Override
 	public Set<Recipe> getRecipes() {
+		log.debug("I'm in the service");
 		Set<Recipe> recipeSet = new HashSet<>();
 		recipeRepository.findAll().iterator().forEachRemaining(recipeSet::add);
 		return recipeSet;
